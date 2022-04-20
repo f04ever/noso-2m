0f0e083694068b91db3b89d6b86ca8e56b57e8a5
f04ever <fzeroforever@gmail.com>
2022-04-20 19:24:45 +0200

    Fixed overflow of total hashes count

--- noso-2m.cpp
+++ noso-2m.cpp
@@ -946,7 +946,7 @@ std::time_t CCommThread::GetMainnetTimestamp( std::size_t min_nodes_count ) {
 }
 
 void CCommThread::_PrintBlockSummary( std::uint32_t blck_no, const std::chrono::duration<double>& elapsed_blck ) {
-    std::uint32_t computed_hashes_count { 0 };
+    std::uint64_t computed_hashes_count { 0 };
     double block_mining_duration { 0. };
     for_each( g_mine_objects.begin(), g_mine_objects.end(), [&](const auto &object){
                  auto summary = object->GetBlockSummary();
