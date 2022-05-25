fd3ff43d8e67dde7071f2dd9221f50cd898f155e
f04ever <86355159+f04ever@users.noreply.github.com>
2022-05-25 23:44:16 +0200

    Update build-release.yml, .gitignore, and 64 more files...

---
+++ md5-c.hpp
@@ -0,0 +1,34 @@
+//https://github.com/Zunawe/md5-c
+//commit 1525b0db1fb608afed8948f3a972d55486e8cb31
+//committed on 20 Nov 2021 
+//with modified
+#ifndef ___MD5_C__HPP___
+#define ___MD5_C__HPP___
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct{
+    uint64_t size;        // Size of input in bytes
+    uint32_t buffer[4];   // Current accumulation of hash
+    uint8_t input[64];    // Input to be used in the next step
+    uint8_t digest[16];   // Result of algorithm
+}MD5Context;
+
+void md5Init(MD5Context *ctx);
+void md5Update(MD5Context *ctx, uint8_t *input, size_t input_len);
+void md5Finalize(MD5Context *ctx);
+void md5Step(uint32_t *buffer, uint32_t *input);
+
+// uint8_t* md5String(char *input);
+// uint8_t* md5File(FILE *file);
+
+uint32_t F(uint32_t X, uint32_t Y, uint32_t Z);
+uint32_t G(uint32_t X, uint32_t Y, uint32_t Z);
+uint32_t H(uint32_t X, uint32_t Y, uint32_t Z);
+uint32_t I(uint32_t X, uint32_t Y, uint32_t Z);
+
+uint32_t rotateLeft(uint32_t x, uint32_t n);
+
+#endif // ___MD5_C__HPP___
