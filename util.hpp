952d138357b3d5ec5e40a6aff3196909ec0f8265
f04ever <fzeroforever@gmail.com>
2022-06-11 13:25:54 +0200

    Restructure source code by splitting the origin file noso-2m.cpp

---
+++ util.hpp
@@ -0,0 +1,11 @@
+#ifndef __NOSO2M_UTIL_HPP__
+#define __NOSO2M_UTIL_HPP__
+#include <string>
+
+std::string lpad( std::string const & s, std::size_t n, char c );
+std::string& ltrim( std::string& s );
+std::string& rtrim( std::string& s );
+bool iequal( std::string const & s1, std::string const & s2 );
+char hashrate_pretty_unit( std::uint64_t count );
+double hashrate_pretty_value( std::uint64_t count );
+#endif // __NOSO2M_UTIL_HPP__
