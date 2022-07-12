bc83f1f87b3d314fc2abfa008d0403672198a505
f04ever <fzeroforever@gmail.com>
2022-07-12 13:50:53 +0200

    Improve hashing

--- hashing.hpp
+++ hashing.hpp
@@ -9,9 +9,9 @@ class CNosoHasher {
     char m_base[19];
     char m_hash[33];
     char m_diff[33];
-    char m_stat[129*128];
+    char m_stat[258];
     MD5Context m_md5_ctx;
-    constexpr static char const hex_dec2char_table[16] {
+    constexpr static char const hex_dec2char_table[] {
 '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
     constexpr static char const hex_char2dec_table[] {
  0,
@@ -22,7 +22,7 @@ class CNosoHasher {
  0,   0,   0,   0,   0,   0,   0,   0,   1,   2,
  3,   4,   5,   6,   7,   8,   9,   0,   0,   0,
  0,   0,   0,   0,  10,  11,  12,  13,  14,  15, };
-    constexpr static std::uint16_t const nosohash_chars_table[505] {
+    constexpr static std::uint16_t const nosohash_chars_table[] {
   0,
   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
   0,   0,   0,   0,   0,   0,   0,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,  48,
@@ -46,10 +46,12 @@ class CNosoHasher {
  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  97,  98,  99, 100,
 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124,
     };
-    static
-    void _ito9a( std::uint32_t n, char * buff );
-    void _hash();
-    void _md5();
+    void _init( char const prefix[10], char const address[32] );
+    void _itoa( std::uint32_t n );
+    void _stat();
+    void _pack();
+    void _md5d();
+    void _diff( char const target[33] );
 public:
     CNosoHasher( char const prefix[10], char const address[32] );
     char const * GetBase( std::uint32_t counter );
